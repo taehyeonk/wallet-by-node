52a0fae8e1e9ce4a63f9df843bbeb3397bc8c8c5
TaeHyeon Kim <xogus9641@naver.com>
2021-06-11 16:12:03 +0900

    url & page 추가

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let express = require('express');
+let express = require('express'); // 웹서버 모듈
 let app = express();
 let router = require('./router/main')(app);
 let port = process.env.PORT || 3000;
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public'));
 
+// 웹서버를 동작시키는 코드
 let server = app.listen(port, function(){
     console.log("Express server has started on port "+ port)
 });
